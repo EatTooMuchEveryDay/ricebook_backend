edeb99c424b2e148084acabce93ff93bb87b6a44
酒酿圆子黏黏汤。 <xrui0310@126.com>
2021-11-05 12:46:12 -0500

    ic 19

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
+const auth = require('./src/auth');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const userSchema = require('./src/userSchema');
+const User = mongoose.model('user', userSchema);
+const connectionString = 'mongodb+srv://new-user1:ricecomp531@cluster0.kcggc.mongodb.net/social';
 
 let articles = [{ id: 0, author: 'Mack', body: 'Post 1' },
 { id: 1, author: 'Jack', body: 'Post 2' },
@@ -8,24 +13,40 @@ let articles = [{ id: 0, author: 'Mack', body: 'Post 1' },
 
 const hello = (req, res) => res.send({ hello: 'world' });
 
+const addUser = (req, res) => {
+    let username = req.params.uname;
+
+    (async () => {
+        const connector = mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        await (connector.then(() => {
+            return new User({
+                username,
+                created: Date.now()
+            }).save();
+        }));
+
+        res.send({ name: username });
+    })();
+};
+
 const getArticles = (req, res) => res.send(articles);
 
-const getArticle = (req, res) => res.send(articles.find((x) => (x.id == req.params.id)));
+const getArticle = (req, res) => res.send(articles[req.params.id]);
 
 const addArticle = (req, res) => {
-    let body = req.body;
-    articles.push({
-        id: Math.floor(Math.random() * 9999999),
-        author: body.author,
-        body: body.body
-    })
+    let post = req.body;
+    let article = { id: articles.length, author: post.author, body: post.body }
+    articles.push(article);
     res.send(articles);
 }
 
-
 const app = express();
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.get('/', hello);
+app.post('/users/:uname', addUser);
+auth(app);
 app.get('/articles', getArticles);
 app.get('/articles/:id', getArticle);
 app.post('/article', addArticle);
